5861466feb504ac2f882142cf74377236c95c0c1
ZJ920 <1622781044@qq.com>
2023-08-29 15:48:34 +0800

    day3-maven

---
+++ Day3_Maven/Maven-Learning/src/main/java/javaweb/maven/day3/MavenDemo1.java
@@ -0,0 +1,10 @@
+package javaweb.maven.day3;
+
+import java.sql.DriverManager;
+
+public class MavenDemo1 {
+    public static void main(String[] args){
+        System.out.println("Hello Maven...");
+
+    }
+}
---
+++ Day3_Maven/Maven-Learning/src/test/java/MavenDemoTest.java
@@ -0,0 +1,6 @@
+
+public class MavenDemoTest {
+    public static void main(String[] args){
+        System.out.println("Hello Maven Test...");
+    }
+}
