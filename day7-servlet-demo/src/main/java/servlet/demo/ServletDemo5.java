1b938755bf06fd232b144d26feb2f2eacfca8974
ZJ920 <1622781044@qq.com>
2023-09-06 19:25:04 +0800

    day7-Servlet

--- day7-servlet-demo/src/main/java/servlet/demo/ServletDemo5.java
+++ day7-servlet-demo/src/main/java/servlet/demo/ServletDemo5.java
@@ -1,13 +1,8 @@
 package servlet.demo;
 
-import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @WebServlet(urlPatterns = "/demo5")
 public class ServletDemo5 extends MyServlet {
